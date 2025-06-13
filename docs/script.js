019492548364a46bc0a9db5daa5010173b25e305
Aizen-0604 <142096004+Aizen-0604@users.noreply.github.com>
2025-06-13 18:09:59 +0530

    Update script.js

--- docs/script.js
+++ docs/script.js
@@ -16,7 +16,7 @@ async function calculate() {
     }
 
     try {
-        const response = await fetch("https://localhost:10000/calculate", {
+        const response = await fetch("https://assign-5mej.onrender.com/api/calculate", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
